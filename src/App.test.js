0ba5b64ccf9b78105d85fa4742118e18465b61c0
NaKfl <nguyenhuugiatri@gmail.com>
2021-06-11 23:37:02 +0700

    Add slack noti

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/react";
-import TodoList from "./components/TodoList";
+import { render } from '@testing-library/react';
+import TodoList from './components/TodoList';
 
-test("title Todo List", () => {
+test('title Todo List', () => {
   const dom = render(<TodoList />);
-  const title = dom.container.getElementsByClassName("title-todo-list");
-  expect(title[0].textContent).toBe(`What's the Plan for Today?`);
+  const title = dom.container.getElementsByClassName('title-todo-list');
+  expect(title[0].textContent).toBe(`What's the Plan for Today`);
 });
