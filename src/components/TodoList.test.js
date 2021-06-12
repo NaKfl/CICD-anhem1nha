23b2800180e3c6de99adfb53333761690e60f90e
huynhthaianhhd <huynhthaianhhd@gmail.com>
2021-06-12 21:51:16 +0700

    Edit test case success

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -14,15 +14,15 @@ test("Todo form define", () => {
 test("title Todo List", () => {
   const dom = render(<TodoList />);
   const title = dom.container.getElementsByClassName("title-todo-list");
-  // expect(title[0].textContent).toBe(`What's the Plan for Today?`);
-  expect(title[0].textContent).toBe(`What's the Plannnn?`);
+  expect(title[0].textContent).toBe(`What's the Plan for Today?`);
+  // expect(title[0].textContent).toBe(`What's the Plannnn?`);
 });
 
 test("prop Todo List", () => {
   const todoList = <TodoList />;
   const props = todoList.props;
-  // expect(props).toMatchObject({});
-  expect(props).toMatchObject({
-    todos: [1,2,3],
-  });
+  expect(props).toMatchObject({});
+  // expect(props).toMatchObject({
+  //   todos: [1,2,3],
+  // });
 });
